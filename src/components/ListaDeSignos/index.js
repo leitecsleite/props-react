cf8e2ceec8af4fa4ddbac515d7d8afd71e022337
Lucas Leite <carvalho1104@hotmail.com>
2021-03-22 22:04:32 -0300

    criando os componentes

---
+++ src/components/ListaDeSignos/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Titulo from '../Titulo';
+import ItemLista from '../ItemLista';
+
+import './estilo.css';
+
+export default function ListaDeSignos() {
+  return (
+    <div>
+      <Titulo />
+
+      <div className="lista">
+        <ItemLista
+            signo="Aquário" dataInicio="21/01"
+            dataFim="19/02" imagem="assets/aquario.jpg"/>
+      </div>
+    </div>
+  )
+}
